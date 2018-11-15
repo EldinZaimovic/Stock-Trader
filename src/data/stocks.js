93981beddc0f2ac99eb47cf816b6dba82d1a63d8
Eldin Zaimovic <eldin@zaimovic.com>
2018-11-16 00:09:15 +0100

    Update stocks.js
    More stocks added

--- src/data/stocks.js
+++ src/data/stocks.js
@@ -1,29 +1,39 @@
 export default [{
-  id: 1,
-  name: "BMW",
-  price: 110
-}, {
-  id: 2,
-  name: "Google",
-  price: 210
-}, {
-  id: 3,
-  name: "Apple",
-  price: 150
-}, {
-  id: 4,
-  name: "Intel",
-  price: 130
-}, {
-  id: 5,
-  name: "Apple",
-  price: 150
-}, {
-  id: 6,
-  name: "Apple",
-  price: 150
-}, {
-  id: 7,
-  name: "Apple",
-  price: 100
-}]
+    id: 1,
+    name: "BMW",
+    price: 110
+  }, {
+    id: 2,
+    name: "Google",
+    price: 210
+  }, {
+    id: 3,
+    name: "Apple",
+    price: 150
+  }, {
+    id: 4,
+    name: "Intel",
+    price: 130
+  }, {
+    id: 5,
+    name: "Amazon",
+    price: 170
+  }, {
+    id: 6,
+    name: "Microsoft",
+    price: 195
+  }, {
+    id: 7,
+    name: "Mercedes-Benz",
+    price: 100
+  }, {
+    id: 8,
+    name: "Bayer",
+    price: 110
+  }, {
+    id: 9,
+    name: "Siemens",
+    price: 230
+  }
+
+]
